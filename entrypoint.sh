f76604e964be35261974a518e5c1295ac8c4c886
Kevin Fu <kevin.fu808@gmail.com>
2022-05-01 21:38:28 -0700

    Adding additional logging to test at what point git is failing

--- entrypoint.sh
+++ entrypoint.sh
@@ -35,8 +35,13 @@ git --version
 # Push the revert commit to the Git repository
 #-----------------------------------------------------------------------
 git remote set-url origin https://x-access-token:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git
-git config --global user.email "revert-commit@github.com"
+
+echo "Setting git configs for email / name."
+
+git config --global user.email "revert-commit@tofuapi.com"
 git config --global user.name "GitHub Revert Commit Action"
+
+echo "Setting git configs for safe directory."
 
 # Workaround for Git Security Vulnerability https://github.com/actions/checkout/issues/760
 git config --global --add safe.directory $GITHUB_WORKSPACE
